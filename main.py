5e5076dee55bda33eca77756da47c78b04d95af0
dbriksza <dbriksza@gmail.com>
2020-08-11 03:10:05 -0400

    can now use special keys for chatbuttonentry option

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from tkinter import *
 import tkinter as tk
 
 root = tk.Tk()
+root.title("Dan's Spam Manager")
+
+# Grid.rowconfigure(root, 0, weight=1)
+Grid.columnconfigure(root, 2, weight=1)
+# Grid.columnconfigure(root, 1, weight=1)
+root.resizable(True,False)
+
+
+#Create & Configure frame 
+# frame=Frame(root)
+# frame.grid(row=0, column=0, sticky=N+S+E+W)
 
 def toggle():
 
@@ -16,17 +27,11 @@ def toggle():
         listeners.append(keyboard.Listener(on_press=on_press))
         listeners[-1].start()
 
-# texts = []
 stringvars = []
 stringentries = []
 hotkeys = []
 hotkeyentries = []
 
-# v = StringVar()
-# e = Entry(root, textvariable=v)
-# e.pack()
-
-# v.set("a default value")
 
 def addCommand():
     stringvars.append(StringVar())
@@ -34,51 +39,44 @@ def addCommand():
     hotkeys.append(StringVar())
     hotkeyentries.append(Entry(root, textvariable=hotkeys[-1]))
     for entry in stringentries:
-        entry.grid(row=stringentries.index(entry) + 3,column=0)
+        entry.grid(row=stringentries.index(entry) + 3,column=0,columnspan=3,sticky=W+E)
         if stringvars[stringentries.index(entry)].get() == "":
             stringvars[stringentries.index(entry)].set("default")
     for entry in hotkeyentries:
-        entry.grid(row=hotkeyentries.index(entry) + 3,column=1)
+        entry.grid(row=hotkeyentries.index(entry) + 3,column=3,sticky=W+E)
         if hotkeys[hotkeyentries.index(entry)].get() == "":
             hotkeys[hotkeyentries.index(entry)].set("add a hotkey")
-    # texts.append()
 
 listeners = []
 
 toggle_btn = tk.Button(text="Toggle", width=12, relief="raised", command=toggle)
-toggle_btn.grid(row=0,column=0)
+toggle_btn.grid(row=0,column=0,pady=10,padx=10)
 add_btn = tk.Button(text="Add a command", width=12, relief="raised", command=addCommand)
 add_btn.grid(row=0,column=1)
-tk.Label(text="Chat Button =>").grid(row=1,column=0)
+tk.Label(text="Chat Button:").grid(row=1,column=0,pady=(0,10))
 chatbutton = StringVar()
 chatButtonEntry = Entry(root, textvariable=chatbutton)
 chatButtonEntry.grid(row=1,column=1)
 tk.Label(text="Text").grid(row=2,column=0)
-tk.Label(text="Hotkey").grid(row=2,column=1)
+tk.Label(text="Hotkey").grid(row=2,column=3)
 
 
 def on_press(key):
     for phrase in stringvars:   
         if key == keyboard.KeyCode.from_char(hotkeys[stringvars.index(phrase)].get()):
-            Controller().press(chatbutton.get())
-            Controller().release(chatbutton.get())
+            try:
+                Controller().press(chatbutton.get())
+            except ValueError:
+                Controller().press(getattr(Key, chatbutton.get()))
+            try:
+                Controller().press(getattr(Key, chatbutton.get()))
+            except ValueError:
+                print(chatbutton.get())
             time.sleep(.1)
             Controller().type(phrase.get())
             time.sleep(.1)
             Controller().press(Key.enter)
             Controller().release(Key.enter)
-    # try:
-    #     print('alphanumeric key {0} pressed'.format(
-    #         key.char))
-    # except AttributeError:
-    #     print('special key {0} pressed'.format(
-    #         key))
 
-# def on_release(key):
-#     if key == keyboard.Key.esc:
-#         # Stop listener
-#         return False
-
-# Collect events until released
 addCommand()
 root.mainloop()
