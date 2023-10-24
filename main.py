d854a7966e790e33e961a33fff197d8992a1cbd4
Dan B <dbriksza@gmail.com>
2023-10-24 14:59:01 -0400

    ability to add commands

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ import tkinter as tk
 from typing import Optional
 from ctypes import wintypes, windll, create_unicode_buffer
 
+class Counter():
+    def __init__(self):
+        self.counter = 0
+
+    def increment(self):
+        self.counter += 1
+
+    def reset(self):
+        self.counter = 0
+
+    def get_value(self):
+        return self.counter
+
 def getForegroundWindowTitle() -> Optional[str]:
     hWnd = windll.user32.GetForegroundWindow()
     length = windll.user32.GetWindowTextLengthW(hWnd)
@@ -33,7 +46,6 @@ root.resizable(True,False)
 # frame.grid(row=0, column=0, sticky=N+S+E+W)
 
 def toggle():
-
     if toggle_btn.config('relief')[-1] == 'sunken':
         toggle_btn.config(relief="raised")
         listeners[-1].stop()
@@ -42,11 +54,33 @@ def toggle():
         listeners.append(keyboard.Listener(on_press=on_press))
         listeners[-1].start()
 
+def toggleAuto(btn):
+    if autoentries[btn].config('relief')[-1] == 'sunken':
+        autoentries[btn].config(relief='raised')
+    elif autoentries[btn].config('relief')[-1] == 'raised':
+        autoentries[btn].config(relief='sunken')
+    if holdentries[btn].config('relief')[-1] == 'sunken':
+        holdentries[btn].config(relief='raised')
+    elif holdentries[btn].config('relief')[-1] == 'raised':
+        holdentries[btn].config(relief='sunken')
+
 stringvars = []
 stringentries = []
 hotkeys = []
 hotkeyentries = []
 
+delays = []
+delayentries = []
+
+autoindexer = Counter()
+
+autos = []
+autoentries = {}
+
+holdindexer = Counter()
+
+holds = []
+holdentries = {}
 
 def addCommand():
     stringvars.append(StringVar())
@@ -62,11 +96,31 @@ def addCommand():
         if hotkeys[hotkeyentries.index(entry)].get() == "":
             hotkeys[hotkeyentries.index(entry)].set("")
 
+def addAuto():
+    autoindexer.increment()
+    buttonindex = autoindexer.get_value()
+    autos.append(buttonindex)
+    delays.append(StringVar())
+    delayentries.append(Entry(root, textvariable=delays[-1]))
+    autoentries[buttonindex] = tk.Button(text="toggle", width=12, relief="raised", command=lambda: toggleAuto(buttonindex))
+    autoentries[buttonindex].grid(row=len(autoentries) + 2,column=4,sticky=W+E)
+    for entry in delayentries:
+        entry.grid(row=delayentries.index(entry) + 3,column=5,sticky=W+E)
+        if delays[delayentries.index(entry)].get() == "":
+            delays[delayentries.index(entry)].set("500")
+
+def addHold():
+    holdindexer.increment()
+    buttonindex = holdindexer.get_value()
+    holds.append(buttonindex)
+    holdentries[buttonindex] = tk.Button(text="toggle", width=12, relief="raised", command=lambda: toggleAuto(buttonindex))
+    holdentries[buttonindex].grid(row=len(autoentries) + 2,column=6,sticky=W+E)
+
 listeners = []
 
 toggle_btn = tk.Button(text="Toggle", width=12, relief="raised", command=toggle)
 toggle_btn.grid(row=0,column=0,pady=10,padx=10)
-add_btn = tk.Button(text="Add a command", width=12, relief="raised", command=addCommand)
+add_btn = tk.Button(text="Add a command", width=12, relief="raised", command=lambda: [addCommand(), addAuto()])
 add_btn.grid(row=0,column=1)
 tk.Label(text="Chat Button:").grid(row=1,column=0,pady=(0,10))
 chatbutton = StringVar()
@@ -74,7 +128,10 @@ chatButtonEntry = Entry(root, textvariable=chatbutton)
 chatButtonEntry.grid(row=1,column=1)
 tk.Label(text="Text").grid(row=2,column=0)
 tk.Label(text="Hotkey").grid(row=2,column=3)
+tk.Label(text="Repeat:").grid(row=2,column=4,pady=(0,10))
+tk.Label(text="Delay:").grid(row=2,column=5,pady=(0,10))
 
+timetostop = {}
 
 def on_press(key):
     for phrase in stringvars:
@@ -94,14 +151,29 @@ def on_press(key):
                         Controller().release(getattr(Key, chatbutton.get()))
                     except AttributeError:
                         None
-                time.sleep(.1)
-                Controller().type(phrase.get())
-                time.sleep(.1)
-                Controller().press(Key.enter)
-                Controller().release(Key.enter)
-                listeners[-1].stop()
-                listeners.append(keyboard.Listener(on_press=on_press))
-                listeners[-1].start()
+                if autoentries[stringvars.index(phrase) + 1].config('relief')[-1] == 'sunken' and (key not in timetostop or timetostop[key] == 'off'):
+                    timetostop[key] = 'on'
+                    listeners[-1].stop()
+                    listeners.append(keyboard.Listener(on_press=on_press))
+                    listeners[-1].start()
+                    while timetostop[key] == 'on':
+                        Controller().type(phrase.get())
+                        time.sleep(int(delays[stringvars.index(phrase)].get()) / 1000)
+                elif timetostop.get(key, None) == 'on':
+                    timetostop[key] = 'off'
+                else: 
+                    time.sleep(.1)
+                    Controller().type(phrase.get())
+                    time.sleep(.1)
+                    Controller().press(Key.enter)
+                    Controller().release(Key.enter)
+                    active = 0
+                    listeners[-1].stop()
+                    listeners.append(keyboard.Listener(on_press=on_press))
+                    listeners[-1].start()
+                    
 
+addAuto()
 addCommand()
+addHold()
 root.mainloop()
